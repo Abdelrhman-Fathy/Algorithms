c2b1db138eef26f55b103244060b38dffab7a4f2
Abdelrhman-Fathy <abdelrhman.fathy@gmail.com>
2019-11-11 21:41:36 +0400

    This is initial commit for data structure package.

---
+++ src/datastructures/compareArrays.java
@@ -0,0 +1,15 @@
+package datastructures;
+
+public class compareArrays {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public static boolean contains(String[] arr1, String[] arr2) {
+		return false;
+		
+	}
+
+}
