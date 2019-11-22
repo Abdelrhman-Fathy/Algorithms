805bf483cc39469c2e34e7fa416be19ccf217e63
Abdelrhman-Fathy <abdelrhman.fathy@gmail.com>
2019-11-22 12:37:52 +0400

    symmertic tree problem implementation

---
+++ src/datastructures/SymmetricTree.java
@@ -0,0 +1,75 @@
+package datastructures;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SymmetricTree {
+
+	public static void main(String[] args) {
+		TreeNode tn = new TreeNode(1);
+		tn.left = new TreeNode(2);
+		tn.right = new TreeNode(2);
+		tn.left.right = new TreeNode(3);
+		tn.left.right = new TreeNode(4);
+		
+		System.out.println(new SymmetricTree().isSymmetric(new TreeNode(0)));
+
+	}
+	
+	public boolean isSymmetric(TreeNode root) {
+        if(root == null)
+            return true;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root.left);
+        q.add(root.right);
+        while(!q.isEmpty()){
+            TreeNode n1 = q.poll();
+            TreeNode n2 = q.poll();
+            if(n1 == null && n2 == null)
+                continue;
+            if(n1 == null || n2 == null)
+                return false;
+            if(n1.val != n2.val)
+                return false;
+            q.add(n1.left);
+            q.add(n2.right);
+            q.add(n1.right);
+            q.add(n2.left);
+        }
+        return true;
+    }
+	
+	public static class TreeNode {
+	    int val;
+	    TreeNode left;
+	    TreeNode right;
+	    TreeNode(int x) { val = x; }
+	} 
+	
+}
+/*
+ * https://leetcode.com/problems/symmetric-tree/
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ 
+
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+ */
+
