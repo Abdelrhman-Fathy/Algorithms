dc6130d7d182876c5c5e1753ed842cfcaadb9c31
Abdelrhman-Fathy <abdelrhman.fathy@gmail.com>
2019-11-22 12:39:15 +0400

    SearchAutocompleteSystem problem solution

---
+++ src/datastructures/SearchAutocompleteSystem.java
@@ -0,0 +1,14 @@
+package datastructures;
+
+public class SearchAutocompleteSystem {
+	
+	public static void main(String[] args) {
+		//System.out.println(new AmazonCriticalConnections().criticalConnections(4, twoDArrayToList(new Integer[][] {{0,1},{1,2},{2,0},{1,3}})));
+		//System.out.println(new AmazonCriticalConnections().criticalConnections(9, twoDArrayToList(new Integer[][] {{1, 2}, {1, 3}, {2, 3}, {3, 4}, {3, 6}, {4, 5}, {6, 7}, {6, 9}, {7, 8}, {8, 9}})));
+		
+		
+		 
+		
+	}
+
+}
